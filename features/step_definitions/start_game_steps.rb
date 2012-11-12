76f618002d0c9d0b9b128ac09bd06bd2121ae9a0
Jeffrey Ng <jeffrey.s.h.ng@gmail.com>
2012-11-12 15:41:30 +0000

    Adding my first scenario
    Adding this scenario
    Scenario: Player decides to play
    Given The wager is £100 to play
    When the player has £1000
    Then take away the wager away from the player's balance

--- features/step_definitions/start_game_steps.rb
+++ features/step_definitions/start_game_steps.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
+
 require "test/unit"
 require "./lib/game"
 require "./lib/player"
+require "./lib/wager"
 
 include Test::Unit::Assertions
 
@@ -15,4 +18,21 @@ end
 
 Then /^the player will be prompted to place a wager$/ do
   assert_equal('Please make your bet', @instruction)
+end
+
+Given /^The wager is £(\d+) to play$/ do |arg1|
+  wager = Wager.new
+  @wager_to_play = wager.what_is_the_wager(100)
+  assert_equal(100,wager.what_is_the_wager(100))
+end
+
+When /^the player has £(\d+)$/ do |arg1|
+  player = Player.new
+  @player_balance = player.player_balance
+  assert_equal(1000,player.player_balance)
+end
+
+Then /^take away the wager away from the player's balance$/ do
+  player = Player.new
+  assert_equal(900,player.update_player_balance(@player_balance,@wager_to_play))
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,10 @@
 class Player
+  @balance = 0
+  def player_balance
+    @balance = 1000
+  end
 
+  def update_player_balance(player_balance,wager_to_play)
+    @balance = player_balance - wager_to_play
+  end
 end
